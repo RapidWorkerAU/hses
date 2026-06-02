0aeab5897927495b9774cf43ae3a095de9d50e92
Ashleigh <ashleigh.phillips@hses.com.au>
2026-06-02 12:52:16 +0800

    commit latest changes to hses

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,52 @@
 ﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  trailingSlash: false,
   turbopack: {
     root: __dirname,
   },
   async redirects() {
     return [
+      {
+        source: "/real-work-examples",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/safety-hub-2026",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/our-safety-bundles",
+        destination: "/document-development",
+        permanent: true,
+      },
+      {
+        source: "/2026-safety-masterclass",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/shop-now__trashed",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/privacy-policy",
+        destination: "/privacy",
+        permanent: true,
+      },
+      {
+        source: "/terms-conditions",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/website-disclaimer",
+        destination: "/disclaimer",
+        permanent: true,
+      },
       {
         source: "/sms-diagnostic/dashboard/:path*",
         destination: "/dashboard/:path*",
